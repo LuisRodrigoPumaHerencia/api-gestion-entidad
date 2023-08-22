3c2237c0a9ea447ec8d9407ce2ac30516d3bd02d
Luis Rodrigo Puma Herencia <U19314535@utp.edu.pe>
2023-08-22 15:27:41 -0500

    ultima vrs fl0

--- src/main/java/com/gestion/apigestionentidad/ApiGestionEntidadApplication.java
+++ src/main/java/com/gestion/apigestionentidad/ApiGestionEntidadApplication.java
@@ -2,12 +2,28 @@ package com.gestion.apigestionentidad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.gestion.apigestionentidad.model.Usuario;
+import com.gestion.apigestionentidad.repository.UsuarioRepository;
 
 @SpringBootApplication
 public class ApiGestionEntidadApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiGestionEntidadApplication.class, args);
+		
+		// Usuario user = new Usuario();
+		// user.setEmail("admin");
+		// user.setNombreUsuario("admin");
+		// user.setIdUsuario(Long.parseLong("1"));
+		// user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+		// ApplicationContext ctx = SpringApplication.run(ApiGestionEntidadApplication.class, args);
+		// UsuarioRepository usuarioRepository = ctx.getBean(UsuarioRepository.class);
+		// usuarioRepository.save(user);
 	}
+
 
 }
